013025b41e30d6263a444d11e4da002d08b4a399
AayushPathak <aayush.pathak15@gmail.com>
2020-10-13 13:36:16 +0530

    Added k8s config files and ported the request to use axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+const axios = require('axios');
 
 class App extends Component {
-  state = {users: []}
-
+  state = {users: []};
   componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    axios.get('/api/users')
+      .then((response) => {
+        this.setState({users: response.data})
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
   }
 
   render() {
@@ -15,7 +20,7 @@ class App extends Component {
       <div className="App">
         <h1>Users</h1>
         {this.state.users.map(user =>
-          <div key={user.id}>{user.name}</div>
+          <div key={user.id}>{user.firstname}</div>
         )}
       </div>
     );
